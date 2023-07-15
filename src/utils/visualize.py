7b0f45542ef0acf8d8028bb1e85808c26f4d4579
Artemiy <artem.lyamin3018@yandex.ru>
2023-07-15 21:22:25 +0300

    Add comments

--- src/utils/visualize.py
+++ src/utils/visualize.py
@@ -11,7 +11,13 @@ from src.config.config import config
 
 def show_images_with_detection(coco: COCO, images: list, number_of_batch: int, ids: list):
     """
-        
+    Function for visualizing images with all objects and their bounding boxes.
+
+    Arguments:
+        coco (COCO): coco object of the entire dataset
+        images (list): images from dataset
+        number_of_batch (int): butch's number in the dataset
+        ids (list): indexes of images in the batch
     """
     for n_image in range(len(ids)):
         image_filepath = f'batch_{number_of_batch}/{ids[n_image]}.jpg'
@@ -39,11 +45,14 @@ def show_images_with_detection(coco: COCO, images: list, number_of_batch: int, i
             plt.axis("off")
             plt.imshow(I)
 
+            # Objects annotations
             annIds = coco.getAnnIds(imgIds=img_id, catIds=[], iscrowd=None)
             anns_sel = coco.loadAnns(annIds)
 
             for ann in anns_sel:
                 color = colorsys.hsv_to_rgb(np.random.random(), 1, 1)
+
+                # Object segmentations
                 for seg in ann['segmentation']:
                     poly = Polygon(np.array(seg).reshape((int(len(seg) / 2)), 2))
                     p = PatchCollection([poly], facecolor=color, edgecolors=color, linewidth=0, alpha=0.4)
@@ -51,6 +60,7 @@ def show_images_with_detection(coco: COCO, images: list, number_of_batch: int, i
                     p = PatchCollection([poly], facecolor='none', edgecolors=color, linewidth=2)
                     ax.add_collection(p)
 
+                # Object bounding box
                 [x, y, w, z] = ann['bbox']
                 print("bbox coordinates:", *[x, y, w, z])
                 rect = Rectangle((x, y), w, z, linewidth=2, edgecolor=color, facecolor='none',
