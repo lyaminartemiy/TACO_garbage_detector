fc86ff857c91529ce0cd11cbf3235410ae5f9526
Artemiy <artem.lyamin3018@yandex.ru>
2023-07-15 17:33:32 +0300

    Add ann_transform.py

---
+++ src/utils/ann_transform.py
@@ -0,0 +1,35 @@
+def transform_image_with_bbox(image: Image, bboxes: list, desired_size: tuple, transform: T, size: tuple):
+    resized_image = torch.FloatTensor(transform(image))
+
+    width, height = size[0], size[1]
+    resized_bboxes = []
+
+    for bbx in bboxes:
+        resized_bbox = [bbx[0] * (desired_size[0] / size[0]),
+                        bbx[1] * (desired_size[1] / size[1]),
+                        bbx[2] * (desired_size[0] / size[0]) + bbx[0] * (desired_size[0] / size[0]),
+                        bbx[3] * (desired_size[1] / size[1]) + bbx[1] * (desired_size[1] / size[1])]
+        resized_bboxes.append(resized_bbox)
+
+    resized_bboxes = torch.as_tensor(resized_bboxes, dtype=torch.float32)
+
+    return resized_image, resized_bboxes
+
+
+def parse_bboxes_and_categories(id_image: int, anns: list):
+    bboxes = []
+    categories = []
+
+    for ann in anns:
+        if ann['image_id'] == id_image:
+            bboxes.append(ann['bbox'])
+            categories.append(ann['category_id'])
+
+    return bboxes, categories
+
+
+def get_id_of_category(id_name: int):
+    for index, label in enumerate(LABELS):
+        if id_name in LABELS[label]:
+            return index + 1
+    return -1
